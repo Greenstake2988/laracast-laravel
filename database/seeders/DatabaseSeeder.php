6cd9250e1de53e9f8b9dbbba8642bc2ba8d8489c
Oscar Daniel Vazquez Alcocer <oscar.va@valladolid.tecnm.mx>
2025-03-12 16:31:15 -0600

    Database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Job;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +16,14 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+//        User::factory()->create([
+//            'first_name' => 'Jhon',
+//            'last_name' => 'Doe',
+//            'email' => 'test@example.com',
+//        ]);
+
+        User::factory(10)->create();
+
+        $this->call(JobSeeder::class);
     }
 }
